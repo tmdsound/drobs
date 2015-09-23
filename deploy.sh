1bb4c21d9ba9b0baabf703c249edf177cd53e979
droboshok <droboshok@yahoo.com>
2015-09-24 00:09:29 +0300

    install 14

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ git checkout master
 git branch -D gh-pages
 git checkout -b gh-pages
 git filter-branch --subdirectory-filter _site/ -f
-git checkout smaster
+git checkout master
 git push --all origin
