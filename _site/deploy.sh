2fddb71fc2845f3bc203d072828d8fe2df3a9a85
droboshok <droboshok@yahoo.com>
2015-09-24 09:09:25 +0300

    13334

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -4,9 +4,9 @@ git checkout source
 git branch -D master
 git checkout -b master
 git filter-branch --subdirectory-filter _site/ -f
-git checkout source
+
 git branch -D gh-pages
 git checkout -b gh-pages
-git filter-branch --subdirectory-filter _site/ -f
+
 git checkout source
 git push --all origin -f
